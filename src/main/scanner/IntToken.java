6a1fb797c142f9abb9e8de1e8f800507d1a8f274
schroedernoah <schronab@uni.edu>
2016-09-21 17:46:11 -0500

    updated inttoken

--- src/main/scanner/IntToken.java
+++ src/main/scanner/IntToken.java
@@ -1,10 +1,18 @@
 
-
 public class IntToken implements Token{
+    
+    private final double MAX = Math.pow(2, 32) - 1;
     String outputString = "Integer ";
+    double intValue;
+    
     public IntToken(String inputInt){
-        outputString += inputInt;
+        intValue = (Double.parseDouble(inputInt)) % MAX;
+        outputString += intValue;
     }
+    public double getIntValue(){
+        return intValue;
+    }
+    @Override
     public String toString(){
         return outputString;
     }

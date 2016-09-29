83a584c26ea66a4356de372c528765dbee4931b8
lindlaaa <lindlaaa@uni.edu>
2016-09-29 07:32:55 -0500

    Added flairp
    Made separate scripts to run just the scanner and parser, as required
    by wallingford.

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 all:
-	javac -d bin src/main/Compiler.java
+	javac -d bin src/main/*.java
+	javac -d bin src/scanner/*.java
 	javadoc -d doc -package src.main src.scanner
 
 clean:
--- src/main/Compiler.java
+++ src/main/Compiler.java
@@ -7,17 +7,5 @@ import src.scanner.*;
  *  where we will run all of the compilation and testing</b>
  */
 public class Compiler{
-
-
-  /**
-   *  <b>Runs the compiler</b>
-   *
-   *  @param args[] List of args, first being path of file to compile
-   */
-  public static void main(String[] args) throws ScanException{
-    Scanner test = new Scanner(args[0]);
-
-    test.takeAllTokens();
-    test.printTokenStrings();
-  }
+    //TODO
 }

f0a9ea2232a5d50605bb03cf06fb13ad66a3cea3
lindlaaa <lindlaaa@uni.edu>
2016-10-04 14:25:34 -0500

    Made some more parse files
    Added very minimal parse skeleton

--- src/parser/Parsetable.java
+++ src/parser/Parsetable.java
@@ -1,4 +1,6 @@
 package src.parser;
+
+import java.util.HashMap;
 /*
 Contains some data-structure that represents our Parse table,
 holding objects [functions?] that act when its block [location]
@@ -9,5 +11,74 @@ is called.
 
 public class Parsetable{
 
-  //TODO
+  private HashMap table = new HashMap();
+}
+
+
+
+
+
+
+
+
+
+
+
+//TODO FIXME The following is what wallingford made
+
+
+import java.util.Stack;
+
+public class ParsingTable
+{
+    private ParseAction[][] table;
+
+    public ParsingTable()
+    {
+         table = new ParseAction[7][10];
+    }
+
+    public ParseAction lookup( NonTerminal symbol, Token token )
+    {
+        int row = convertNonTerminal( symbol );
+        int col = convertToken( token  );
+
+        return table[row][col];
+    }
+
+    // ------------------------------------------------------------------
+
+    public void add( NonTerminal symbol, Token token, ParseAction action )
+                throws Exception
+    {
+        int row = convertNonTerminal( symbol );
+        int col = convertToken( token  );
+
+        table[row][col] = action;
+    }
+
+    public String toString()
+    {
+        return table.toString();
+    }
+
+    // ------------------------------------------------------------------
+
+    private int convertNonTerminal( NonTerminal symbol )
+    {
+        if (symbol.equals( NonTerminal.Program )       ) return 0;
+        if (symbol.equals( NonTerminal.Declarations )  ) return 1;
+        if (symbol.equals( NonTerminal.Declaration )   ) return 2;
+        if (symbol.equals( NonTerminal.Statements )    ) return 3;
+        if (symbol.equals( NonTerminal.Statement )     ) return 4;
+        if (symbol.equals( NonTerminal.ExpressionTail )) return 5;
+        if (symbol.equals( NonTerminal.Value )         ) return 6;
+
+        return 0;
+    }
+
+    private int convertToken( Token token )
+    {
+        return token.type();
+    }
 }

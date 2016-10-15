eeba78c3e1ebec1ce1536e18b66271b58c63e2c1
schroedernoah <schronab@uni.edu>
2016-10-15 13:29:30 -0500

    semAction are assigned to rules
    the semActions are given a set amount of items they will need to pop,
    and they are placed in the rules but are commented out. Notice that
    there is also a semAction.ActionNumber, semAction.ActionBoolean, and a
    semAction.ActionBoolean

--- src/parser/SemAction.java
+++ src/parser/SemAction.java
@@ -3,92 +3,98 @@ package src.parser;
 public class SemAction
 {
     public static final SemAction Action01 =
-                        new SemAction(1);
+                        new SemAction(7);
     public static final SemAction Action02 =
                         new SemAction(2);
     public static final SemAction Action03 =
-                        new SemAction(3);
-    public static final SemAction Action04 =
-                        new SemAction(4);
-    public static final SemAction Action05 =
-                        new SemAction(5);
-    public static final SemAction Action06 =
-                        new SemAction(6);
-    public static final SemAction Action07 =
-                        new SemAction(7);
-    public static final SemAction Action08 =
-                        new SemAction(8);
-    public static final SemAction Action09 =
                         new SemAction(9);
+    public static final SemAction Action04 =
+                        new SemAction(1);
+    public static final SemAction Action05 =
+                        new SemAction(2);
+    public static final SemAction Action06 =
+                        new SemAction(1);
+    public static final SemAction Action07 =
+                        new SemAction(3);
+    public static final SemAction Action08 =
+                        new SemAction(3);
+    public static final SemAction Action09 =
+                        new SemAction(2);
     public static final SemAction Action10 =
-                        new SemAction(10);
+                        new SemAction(2);
     public static final SemAction Action11 =
-                        new SemAction(11);
+                        new SemAction(1);
     public static final SemAction Action12 =
-                        new SemAction(12);
+                        new SemAction(1);
     public static final SemAction Action13 =
-                        new SemAction(13);
+                        new SemAction(2);
     public static final SemAction Action14 =
-                        new SemAction(14);
+                        new SemAction(2);
     public static final SemAction Action15 =
-                        new SemAction(15);
+                        new SemAction(1);
     public static final SemAction Action16 =
-                        new SemAction(16);
+                        new SemAction(2);
     public static final SemAction Action17 =
-                        new SemAction(17);
+                        new SemAction(2);
     public static final SemAction Action18 =
-                        new SemAction(18);
+                        new SemAction(2);
     public static final SemAction Action19 =
-                        new SemAction(19);
+                        new SemAction(2);
     public static final SemAction Action20 =
-                        new SemAction(20);
+                        new SemAction(2);
     public static final SemAction Action21 =
-                        new SemAction(21);
+                        new SemAction(2);
     public static final SemAction Action22 =
-                        new SemAction(22);
+                        new SemAction(2);
     public static final SemAction Action23 =
-                        new SemAction(23);
+                        new SemAction(2);
     public static final SemAction Action24 =
-                        new SemAction(24);
+                        new SemAction(6);
     public static final SemAction Action25 =
-                        new SemAction(25);
+                        new SemAction(2);
     public static final SemAction Action26 =
-                        new SemAction(26);
+                        new SemAction(2);
     public static final SemAction Action27 =
-                        new SemAction(27);
+                        new SemAction(1);
     public static final SemAction Action28 =
-                        new SemAction(28);
+                        new SemAction(2);
     public static final SemAction Action29 =
-                        new SemAction(29);
+                        new SemAction(3);
     public static final SemAction Action30 =
-                        new SemAction(30);
+                        new SemAction(3);
     public static final SemAction Action31 =
-                        new SemAction(31);
+                        new SemAction(1);
     public static final SemAction Action32 =
-                        new SemAction(32);
+                        new SemAction(2);
     public static final SemAction Action33 =
-                        new SemAction(33);
+                        new SemAction(3);
     public static final SemAction Action34 =
-                        new SemAction(34);
+                        new SemAction(1);
     public static final SemAction Action35 =
-                        new SemAction(35);
+                        new SemAction(1);
     public static final SemAction Action36 =
-                        new SemAction(36);
+                        new SemAction(5);
+    public static final SemAction ActionIdentifier =
+                        new SemAction(1);
+    public static final SemAction ActionNumber =
+                        new SemAction(1);
+    public static final SemAction ActionBoolean =
+                        new SemAction(1);						
 					
-    private int typeName;
+    private int itemsToPop;
 
-    private SemAction( int semanticActionNumber )
+    private SemAction( int itemsToPop )
     {
-        typeName = semanticActionNumber;
+        this.itemsToPop = itemsToPop;
     }
 	
 	public int getSemanticAction(){
-		return typeName;
+		return itemsToPop;
 	}
 
     @Override
     public String toString()
     {
-        return typeName.toString();
+        return itemsToPop.toString();
     }
 }

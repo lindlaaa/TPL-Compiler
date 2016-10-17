fe64776a19384b1a61c8a926b4f9150920f84800
schroedernoah <schronab@uni.edu>
2016-10-17 15:17:25 -0500

    Update SemanticNode.java

--- src/parser/SemanticNode.java
+++ src/parser/SemanticNode.java
@@ -3,18 +3,30 @@ package src.parser;
 @SuppressWarnings("unchecked")
 public class  SemanticNode
 {
-	private SemanticAction nodeType;
+  private SemanticAction nodeType;
   private Tree tree;
 
   public SemanticNode( SemanticAction inputNodeType )
   {
-    nodeType = inputNodeType;
+  /*switch(inputNodeType)
+      case:SemanticAction.Program
+        this.nodeType = new ProgramNode();
+	break;
+      case:...
+        this.nodeType = new ...Node();
+        break;
+	. . .
+  */
     createTree();
   }
 
   //Init the tree this node contains
   private void createTree(){
     tree = new Tree(this);
+  }
+  
+  public SemanticAction getNodeType(){
+	  return this.nodeType;
   }
 
   //recieves input node from the top of the semantic stack.
@@ -39,9 +51,29 @@ public class  SemanticNode
 	//  return tempString;
     //}
 }
+
+class ProgramNode(){
+  private ruleList[] = {new IdentifierNode(),
+		        new FormalsNode(),
+                        new DefinitionsNode(),
+                        new BodyNode()
+                      } );	
+  public ProgramNode();
+ 
+  private void getChildren(){
+    if(semanticStack.peek().getNodeType() instanceof IdentifierNode){
+      SemanticNode.this.tree.addleaf(semanticStack.pop());	  
+      for(int i = i; i < rulesList.length(); i++){
+        if(semanticStack.peek().getNodeType() instanceof rulesList[i]){
+	  SemanticNode.this.tree.addleaf(semanticStack.pop());	  
+	 }
+      }
+    }//else do nothing?
+  }
+}
 /*
 package src.parser;
-
+ 
 import java.util.Stack;
 
 public class SemNode
@@ -55,17 +87,17 @@ public class SemNode
 	public SemAction getNodeType(){
 	  return this.nodeType;
 	}
-
+	
 	public void addElement(T inputAction){
 	  this.nodeBody.push(inputAction);
 	}
-
+	
 	public T removeElement(){
 	  return this.nodeBody.pop();
 	}
-
+	
 	public int numberOfElementsToPop(){
-	  return this.nodeType.getSemanticAction();
+	  return this.nodeType.getSemanticAction();	
 	}
 
     //@Override
@@ -73,7 +105,7 @@ public class SemNode
 	//  String tempString;
 	//  for(int i=0; i < nodeBody.length(); i++)
 	//  {
-	//	  tempstring += nodeBody.pop().toString();
+	//	  tempstring += nodeBody.pop().toString(); 
 	//  }
 	//  return tempString;
     //}

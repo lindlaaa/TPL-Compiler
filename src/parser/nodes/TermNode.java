54f8e0e729549ffb3bc1e2ad020267722ae5b5c1
schroedernoah <schronab@uni.edu>
2016-10-20 16:53:39 -0500

    Update TermNode.java

--- src/parser/nodes/TermNode.java
+++ src/parser/nodes/TermNode.java
@@ -5,15 +5,20 @@ import src.parser.*;
 public class TermNode extends SemanticNode{
   @Override
   public void setChildren(){
-    if(TableDrivenParser.semanticStack.peek() instanceof FactorIfNode ||
-	     TableDrivenParser.semanticStack.peek() instanceof FactorNotNode ||
-       TableDrivenParser.semanticStack.peek() instanceof FactorIDNode){
-      this.addChild(TableDrivenParser.semanticStack.pop());
-	  }
-    if(TableDrivenParser.semanticStack.peek() instanceof TermPrimeAndNode ||
-	     TableDrivenParser.semanticStack.peek() instanceof TermPrimeTimesNode ||
-	     TableDrivenParser.semanticStack.peek() instanceof TermPrimeDivideNode){
-      this.addChild(TableDrivenParser.semanticStack.pop());
+    if(TableDrivenParser.semanticStack.peek() instanceof FactorIfNode){	
+      this.addChild((FactorIfNode)TableDrivenParser.semanticStack.pop());	
+	}else if(TableDrivenParser.semanticStack.peek() instanceof FactorNotNode){
+      this.addChild((FactorNotNode)TableDrivenParser.semanticStack.pop());		
+	}else if(TableDrivenParser.semanticStack.peek() instanceof FactorIDNode){
+      this.addChild((FactorIDNode)TableDrivenParser.semanticStack.pop());
+	}
+	  	  
+    if(TableDrivenParser.semanticStack.peek() instanceof TermPrimeAndNode){
+	  this.addChild((TermPrimeAndNode)TableDrivenParser.semanticStack.pop());	
+	}else if(TableDrivenParser.semanticStack.peek() instanceof TermPrimeTimesNode){
+	  this.addChild((TermPrimeTimesNode)TableDrivenParser.semanticStack.pop());	
+	}else if(TableDrivenParser.semanticStack.peek() instanceof TermPrimeDivideNode){
+      this.addChild((TermPrimeDivideNode)TableDrivenParser.semanticStack.pop());
     }
   }
 

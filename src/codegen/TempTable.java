681ffc6b145ad9777e2db010b4487cc52a7d6245
schroedernoah <schronab@uni.edu>
2016-12-09 10:05:54 -0600

    Add files via upload

---
+++ src/codegen/TempTable.java
@@ -0,0 +1,50 @@
+
+package project5;
+
+import java.util.*;
+
+public class TempTable {
+    
+    Map<String, tempUsage> tempTable = new HashMap();
+    
+    public int getVal(String varName)throws Exception{
+        if(this.tempTable.containsKey(varName)){
+            return this.tempTable.get(varName).getIntVal();
+        }else{
+            throw new Exception("getVal() Error: there is no value for the " + varName);
+        }
+    }
+    
+    public void setVal(String varName, int inputVal){
+        tempUsage usage = new tempUsage();
+        usage.setIntVal(inputVal);
+        this.tempTable.put(varName,usage);
+    }
+    
+    public int getLastRegister(String varName)throws Exception{
+        if(this.tempTable.containsKey(varName)){
+            return this.tempTable.get(varName).getReg();
+        }else{
+            throw new Exception("getVal() Error: there is no value for the " + varName);
+        }
+    }    
+    public void setLastRegister(String varName, int inputReg){
+        tempUsage usage = new tempUsage();
+        usage.setReg(inputReg);
+        this.tempTable.put(varName,usage);
+    }   
+ 
+    public boolean isUsed(String varName)throws Exception{
+        if(this.tempTable.containsKey(varName)){
+            return this.tempTable.get(varName).inUse();
+        }else{
+            throw new Exception("getVal() Error: there is no value for the " + varName);
+        }
+    }    
+    public void setLastRegister(String varName){
+        tempUsage usage = new tempUsage();
+        usage.setInUse();
+        this.tempTable.put(varName,usage);
+    }       
+    
+}
---
+++ src/codegen/tempUsage.java
@@ -0,0 +1,28 @@
+
+package project5;
+
+public class tempUsage {
+    int value = Integer.MIN_VALUE;
+    int assignedTo = -1;
+    boolean inUse = false;
+    
+    public int getIntVal(){
+        return this.value;
+    }
+    public void setIntVal(int newVal){
+        this.value = newVal;
+    }
+    public int getReg(){
+        return this.assignedTo;
+    }
+    public void setReg(int newReg){
+        this.assignedTo = newReg;
+    }    
+    public boolean inUse(){
+        return this.inUse;
+    }
+    public void setInUse(){
+        this.inUse = true;
+    }   
+    
+}
